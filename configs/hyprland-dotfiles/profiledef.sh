35ad54db1455e9c3d66d0d391b56c0b7698c0c31
gregorytrentmartinjr <110754385+gregorytrentmartinjr@users.noreply.github.com>
2026-03-28 06:29:06 -0500

    Revert "add auto_scale.sh to profiledef.sh so it works on the live iso"
    This reverts commit 8577ff059258d702a028169a92e63609e467a828.

--- configs/hyprland-dotfiles/profiledef.sh
+++ configs/hyprland-dotfiles/profiledef.sh
@@ -35,7 +35,6 @@ file_permissions=(
   ["/usr/local/bin/sddm-bg-helper"]="0:0:755"
   ["/usr/local/bin/cleanup-desktop-entries"]="0:0:755"
   ["/usr/local/bin/power-key-helper"]="0:0:755"
-  ["/etc/skel/.config/hypr/hyprland/scripts/auto_scale.sh"]="0:0:755"
   ["/etc/skel/.config/hypr/scripts/init-qs.sh"]="0:0:755"
 
   # Calamares autostart (XDG autostart — read by dex/autostart managers)
